871938a868dabdd74b6bb64ec176333331cf3d3d
Sean Kelliher <sbkelliher@icloud.com>
2023-02-12 13:55:19 -0500

    Add the server.js file, the Express server.

---
+++ server.js
@@ -0,0 +1,69 @@
+// ========================
+// Packages
+// ========================
+const express = require("express");
+const app = express();
+
+// ========================
+// Middlewares
+// ========================
+app.use(express.static(__dirname + "/dist/")); // For Heroku deployment.
+
+// ========================
+// Routes
+// ========================
+//const token = process.env.APP_TOKEN; // In Heroku, "Open Data" token kept here.
+
+app.get("/", (req, res) => {
+    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?perp_sex=M&vic_sex=F")
+    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=boro,perp_sex")
+    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=boro,occur_date&$where=vic_sex=='F'")
+    fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=incident_key,occur_time,boro,loc_of_occur_desc,statistical_murder_flag,perp_age_group,perp_sex,perp_race,vic_age_group,vic_sex,vic_race")
+        .then(res => res.json())
+        .then(data => {
+            res.send({ trends: data });
+        })
+        .catch(error => console.error(error));
+});
+
+app.get("/scenes", (req, res) => {
+    fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=incident_key,occur_date,occur_time,boro,loc_of_occur_desc,precinct,loc_classfctn_desc,location_desc,statistical_murder_flag")
+        .then(res => res.json())
+        .then(data => {
+            res.send({ scenes: data });
+        })
+        .catch(error => console.error(error));
+});
+
+app.get("/vics", (req, res) => {
+    fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=incident_key,statistical_murder_flag,vic_age_group,vic_sex,vic_race")
+        .then(res => res.json())
+        .then(data => {
+            res.send({ vics: data });
+        })
+        .catch(error => console.error(error));
+});
+
+app.get("/perps", (req, res) => {
+    fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=incident_key,perp_age_group,perp_sex,perp_race")
+        .then(res => res.json())
+        .then(data => {
+            res.send({ perps: data });
+        })
+        .catch(error => console.error(error));
+});
+
+app.get(/.*/, function(req, res) {
+    res.sendFile(__dirname + "/dist/index.html");
+});
+
+// ========================
+// Listen
+// ========================
+/*app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`);
+});*/
+
+app.listen(4040, () => {
+    console.log("Server listening on port 4040");
+});
