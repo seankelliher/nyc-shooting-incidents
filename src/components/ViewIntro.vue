365d1ecb074b7927a0f45620d8f5acbd9035ea26
Sean Kelliher <sbkelliher@icloud.com>
2023-02-25 17:30:34 -0500

    Add 'ViewIntro' component.

---
+++ src/components/ViewIntro.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="view-intro">
+        <slot>
+            total shooting incidents unavailable
+        </slot>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "ViewIntro"
+    /*props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }*/
+};
+</script>
