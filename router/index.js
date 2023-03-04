ac3bb6aec0acde95cd4a1d92606fa75988fdf3fd
Sean Kelliher <sbkelliher@icloud.com>
2023-03-04 11:07:22 -0500

    Add 'NotFound.vue' view and add it to router.

--- router/index.js
+++ router/index.js
@@ -3,6 +3,7 @@ import ShootingTrends from "../src/views/ShootingTrends.vue";
 import ShootingScenes from "../src/views/ShootingScenes.vue";
 import ShootingVics from "../src/views/ShootingVics.vue";
 import ShootingPerps from "../src/views/ShootingPerps.vue";
+import NotFound from "../src/views/NotFound.vue";
 
 const routes = [
     {
@@ -24,6 +25,11 @@ const routes = [
         path: "/perps",
         name: "ShootingPerps",
         component: ShootingPerps
+    },
+    {
+        path: "/:catchAll(.*)",
+        name: "NotFound",
+        component: NotFound
     }
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+
+        <p>This page does not exist.</p>
+
+    </section>
+
+</template>
+
+<script>
+
+export default {
+    name: "ShootingVics",
+    data() {},
+    created() {},
+    methods: {},
+    components: {}
+};
+</script>
