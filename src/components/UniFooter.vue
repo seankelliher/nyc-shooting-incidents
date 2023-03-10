167c94b99330f74447d3a7041589698e527c27c2
Sean Kelliher <sbkelliher@icloud.com>
2023-03-10 16:44:36 -0500

    Add text to footer.

--- src/components/UniFooter.vue
+++ src/components/UniFooter.vue
@@ -1,5 +1,6 @@
 <template>
     <footer>
+        <p>Shooting incident raw data  at <a href="https://data.cityofnewyork.us/Public-Safety/NYPD-Shooting-Incident-Data-Year-To-Date-/5ucz-vwe8" class="text-link" target="_blank">NYC Open Data</a>.</p>
     </footer>
 </template>
 
