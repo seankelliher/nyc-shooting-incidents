dcd0cd14298b042732b39196e9cc4523df9f385b
Sean Kelliher <sbkelliher@icloud.com>
2023-03-11 13:44:16 -0500

    Modify fetch URL. Trouble shooting why data not appearing after adding APP_TOKEN.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get("/trends", (req, res) => {
     // fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?perp_sex=M&vic_sex=F")
     // fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=boro,perp_sex")
     // fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=boro,occur_date&$where=vic_sex=='F'")
-    fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$$app_token=${token}&select=incident_key,occur_date,statistical_murder_flag&$order=occur_date ASC&$limit=3000`)
+    fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$$app_token=${token}$select=incident_key,occur_date,statistical_murder_flag&$order=occur_date ASC&$limit=3000`)
         .then(res => res.json())
         .then(data => {
             res.send({ trends: data });
@@ -27,7 +27,7 @@ app.get("/trends", (req, res) => {
 });
 
 app.get("/scenes", (req, res) => {
-    fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$$app_token=${token}&select=incident_key,occur_time,boro,loc_of_occur_desc,loc_classfctn_desc,statistical_murder_flag&$limit=3000`)
+    fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$$app_token=${token}$select=incident_key,occur_time,boro,loc_of_occur_desc,loc_classfctn_desc,statistical_murder_flag&$limit=3000`)
         .then(res => res.json())
         .then(data => {
             res.send({ scenes: data });
@@ -36,7 +36,7 @@ app.get("/scenes", (req, res) => {
 });
 
 app.get("/vics", (req, res) => {
-    fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$$app_token=${token}&select=incident_key,statistical_murder_flag,vic_age_group,vic_sex,vic_race&$limit=3000`)
+    fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$$app_token=${token}$select=incident_key,statistical_murder_flag,vic_age_group,vic_sex,vic_race&$limit=3000`)
         .then(res => res.json())
         .then(data => {
             res.send({ vics: data });
@@ -45,7 +45,7 @@ app.get("/vics", (req, res) => {
 });
 
 app.get("/perps", (req, res) => {
-    fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$$app_token=${token}&select=incident_key,statistical_murder_flag,perp_age_group,perp_sex,perp_race&$limit=3000`)
+    fetch(`https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$$app_token=${token}$select=incident_key,statistical_murder_flag,perp_age_group,perp_sex,perp_race&$limit=3000`)
         .then(res => res.json())
         .then(data => {
             res.send({ perps: data });
