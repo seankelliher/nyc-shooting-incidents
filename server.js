ce5a7fc62bcd440da3745c6c0f3b024ee8714f92
Sean Kelliher <sbkelliher@icloud.com>
2023-03-11 12:40:51 -0500

    Modify order of 'app.get' requests. Troubleshooting.

--- server.js
+++ server.js
@@ -14,18 +14,6 @@ app.use(express.static(__dirname + "/dist/")); // For Heroku deployment.
 // ========================
 //const token = process.env.APP_TOKEN; // In Heroku, "Open Data" token kept here.
 
-app.get("/", (req, res) => {
-    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?perp_sex=M&vic_sex=F")
-    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=boro,perp_sex")
-    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=boro,occur_date&$where=vic_sex=='F'")
-    fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=incident_key,occur_date,statistical_murder_flag&$order=occur_date ASC&$limit=3000")
-        .then(res => res.json())
-        .then(data => {
-            res.send({ trends: data });
-        })
-        .catch(error => console.error(error));
-});
-
 app.get("/scenes", (req, res) => {
     fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=incident_key,occur_time,boro,loc_of_occur_desc,loc_classfctn_desc,statistical_murder_flag&$limit=3000")
         .then(res => res.json())
@@ -49,6 +37,18 @@ app.get("/perps", (req, res) => {
         .then(res => res.json())
         .then(data => {
             res.send({ perps: data });
+        })
+        .catch(error => console.error(error));
+});
+
+app.get("/", (req, res) => {
+    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?perp_sex=M&vic_sex=F")
+    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=boro,perp_sex")
+    //fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=boro,occur_date&$where=vic_sex=='F'")
+    fetch("https://data.cityofnewyork.us/resource/5ucz-vwe8.json?$select=incident_key,occur_date,statistical_murder_flag&$order=occur_date ASC&$limit=3000")
+        .then(res => res.json())
+        .then(data => {
+            res.send({ trends: data });
         })
         .catch(error => console.error(error));
 });
