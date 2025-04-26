0c1ddeb20c8881104e9673489019f279a4aab3a5
Sean Kelliher <sbkelliher@icloud.com>
2025-04-26 14:56:01 -0400

    Remove RibbonPart. No longer needed.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup>
 import HeaderPart from "./components/HeaderPart.vue";
-import RibbonPart from "./components/RibbonPart.vue";
 import ShootingIncidents from "./components/ShootingIncidents.vue";
 import FooterPart from "./components/FooterPart.vue";
 </script>
 
 <template>
     <div id="container">
-        <HeaderPart headline="Open Data" />
-        <RibbonPart headline="Shooting Incidents" />
+        <HeaderPart />
         <ShootingIncidents />
         <FooterPart />
     </div>
