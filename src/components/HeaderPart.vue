0bc2d79a0826bcb708373e52951bd3926e5fd3e3
Sean Kelliher <sbkelliher@icloud.com>
2025-04-26 14:57:49 -0400

    Remove 311 and NYC websites link.

--- src/components/HeaderPart.vue
+++ src/components/HeaderPart.vue
@@ -1,10 +1,4 @@
 <script setup>
-defineProps({
-    headline: {
-        type: String,
-        required: true
-    }
-});
 </script>
 
 <template>
@@ -15,18 +9,12 @@ defineProps({
                 <figcaption hidden>logo for the New York City government</figcaption>
             </figure>
             <div class="pipe">|</div>
-            <div class="header-text">
-                <a href="https://opendata.cityofnewyork.us" target="_blank"><h1>{{ headline }}</h1></a>
+            <div class="items-text">
+                <a href="https://opendata.cityofnewyork.us" target="_blank">Open Data</a>
             </div>
         </div>
         <div class="items-right">
-            <div class="header-text">
-                <a href="https://www.nyc.gov/311/index.page" target="_blank">311</a>
-            </div>
-            <div class="pipe">|</div>
-            <div class="header-text">
-                <a href="https://www.nyc.gov/home/search/index.page" target="_blank">Search all NYC.gov websites</a>
-            </div>
+            <h1>YTD Shooting Incidents in NYC</h1>
         </div>
     </header>
 </template>
